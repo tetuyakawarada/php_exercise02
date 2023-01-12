9b69b24b8d6de62c5d60dd6202ff58e5e203bb69
tetuyakawarada <tetuya.kawarada@gmail.com>
2023-01-12 21:43:30 +0900

    01修了

---
+++ php_exercise02/07/01_form2.php
@@ -0,0 +1,50 @@
+<?php
+
+$stylists = [
+    'スタイリスト' => 'Takashi',
+    'ハイスタイリスト' => 'Ken',
+    'トップスタイリスト' => 'Kyoutaro'
+];
+$select_stylist = '';
+
+// ここにコードを追記
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $select_stylist = $_POST['stylist'];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>埋め込み</title>
+</head>
+
+<body>
+    <h1>希望する美容師のランクを選んでください</h1>
+    <form action="" method="post">
+        <select name="stylist">
+            <?php
+            foreach ($stylists as $key => $stylist) {
+                echo "<option value='{$key}'>{$key}</option>";
+            }
+            ?>
+        </select>
+        <br>
+        <input type="submit" value="送信">
+    </form>
+
+    <?php if ($select_stylist) : ?>
+        <p>あなたの担当は <?= $stylists[$select_stylist] ?> です</p>
+    <?php endif; ?>
+
+
+    <p>
+    </p>
+</body>
+
+</html>
